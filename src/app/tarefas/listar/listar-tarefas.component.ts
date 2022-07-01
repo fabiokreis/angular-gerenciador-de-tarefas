7c1367d5999a36637fbcc95c703a4c430b96f658
Fabio Kreis <fabiok@mmb026102cps.local>
2022-07-01 18:49:34 -0300

    Implementando remover tarefa

--- src/app/tarefas/listar/listar-tarefas.component.ts
+++ src/app/tarefas/listar/listar-tarefas.component.ts
@@ -20,4 +20,12 @@ export class ListarTarefasComponent implements OnInit {
   listarTodos(): Tarefa[] {
     return this.tarefaService.listarTodos();
   }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
 }
